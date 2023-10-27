faa04f1f7349d91d8550cb8abb886b80c4d593ed
Dawin <dawin002@naver.com>
2023-10-27 17:29:45 +0900

    "test(baseball-mentor): 테스트 코드 리펙토링

--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -2,6 +2,8 @@ package baseball.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,21 +19,29 @@ class RefereeTest {
         referee = new Referee();
     }
 
-    @Test
-    void 스트라이크3() {
-        String result = referee.compare(ANSWER, Arrays.asList(1, 2, 3));
-        assert (result).equals("0볼 3스트라이크");
+    @ParameterizedTest
+    @CsvSource({"1,2,3,0볼 3스트라이크", "6,7,8,낫싱", "2,3,1,3볼 0스트라이크"})
+    public void compare(int number1, int number2, int number3, String expected) {
+        String actual = referee.compare(ANSWER, Arrays.asList(number1, number2, number3));
+        assert (actual).equals(expected);
     }
-
-    @Test
-    void 낫싱() {
-        String result = referee.compare(ANSWER, Arrays.asList(6, 7, 8));
-        assert (result).equals("낫싱");
-    }
-
-    @Test
-    void 볼3() {
-        String result = referee.compare(ANSWER, Arrays.asList(2, 3, 1));
-        assert (result).equals("3볼 0스트라이크");
-    }
+//    아래 테스트들을 위 테스트 하나로 합칠 수 있음
+//
+//    @Test
+//    void 스트라이크3() {
+//        String result = referee.compare(ANSWER, Arrays.asList(1, 2, 3));
+//        assert (result).equals("0볼 3스트라이크");
+//    }
+//
+//    @Test
+//    void 낫싱() {
+//        String result = referee.compare(ANSWER, Arrays.asList(6, 7, 8));
+//        assert (result).equals("낫싱");
+//    }
+//
+//    @Test
+//    void 볼3() {
+//        String result = referee.compare(ANSWER, Arrays.asList(2, 3, 1));
+//        assert (result).equals("3볼 0스트라이크");
+//    }
 }
