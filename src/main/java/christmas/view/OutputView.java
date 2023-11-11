005ecbf3ff93e207b4d8fe544fadf774cfd18fc9
Dawin <dawin002@naver.com>
2023-11-12 02:34:21 +0900

    feat(OutputView): 방문 날짜 입력 메시지 출력 기능 추가
    방문 날짜 입력 메시지 출력 기능 추가
    - printReadDate()
    - "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)" 출력

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,10 @@
+package christmas.view;
+
+public class OutputView {
+    public OutputView() {
+    }
+
+    public void printReadDate() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+    }
+}
