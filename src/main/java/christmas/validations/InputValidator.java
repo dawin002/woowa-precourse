642d091fe013ccfc514c8ae491777bfc382fa349
Dawin <dawin002@naver.com>
2023-11-12 17:11:56 +0900

    refactor(InputValidator): 입력값 검증 기능 수정
    입력값 검증 기능 반환 타입 boolean으로 수정:
    - isEmpty()
    - isBlank()
    - isNotNumber()

--- src/main/java/christmas/validations/InputValidator.java
+++ src/main/java/christmas/validations/InputValidator.java
@@ -1,26 +1,21 @@
 package christmas.validations;
 
-import java.util.List;
-
 public class InputValidator {
 
-    public void validateIsEmpty(String input) {
-        if (input.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
+    public boolean isEmpty(String input) {
+        return input.isEmpty();
     }
 
-    public void validateIsBlank(String input) {
-        if (input.isBlank()) {
-            throw new IllegalArgumentException();
-        }
+    public boolean isBlank(String input) {
+        return input.isBlank();
     }
 
-    public void validateIsNumber(String input) {
+    public boolean isNotNumber(String input) {
         for (char number : input.toCharArray()) {
             if ((int) number < (int) '0' || (int) number > (int) '9') {
-                throw new IllegalArgumentException();
+                return true;
             }
         }
+        return false;
     }
 }
