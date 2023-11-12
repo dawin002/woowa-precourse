1e32e9f1d9e4787b990a52e67c6725110e72fb60
Dawin <dawin002@naver.com>
2023-11-12 18:42:15 +0900

    refactor(InputValidator): 입력 검증 기능 수정
    날짜 입력값 검증 기능 InputView 클래스에서 이동:
    - validateInputDate()
    주문 입력값 검증 기능 InputView 클래스에서 이동:
    - validateInputOrder()
    문자열 검증 기능 제거:
    - isEmpty()
    - isBlank()
    - isNotContains()
    - 제거 사유: 코드 가독성을 위해

--- src/main/java/christmas/validations/InputValidator.java
+++ src/main/java/christmas/validations/InputValidator.java
@@ -1,25 +1,39 @@
 package christmas.validations;
 
 public class InputValidator {
-
-    public boolean isEmpty(String input) {
-        return input.isEmpty();
+    public void validateDate(String date) {
+        if (date.isBlank()
+                || isNotNumber(date)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
     }
 
-    public boolean isBlank(String input) {
-        return input.isBlank();
+    public void validateOrder(String order) {
+        if (order.isBlank()
+                || !order.contains("-")) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
     }
 
-    public boolean isNotNumber(String input) {
+    public void validateMenu(String menu) {
+        if (menu.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public void validateQuantity(String quantity) {
+        if (quantity.isBlank()
+                || isNotNumber(quantity)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private boolean isNotNumber(String input) {
         for (char number : input.toCharArray()) {
             if ((int) number < (int) '0' || (int) number > (int) '9') {
                 return true;
             }
         }
         return false;
-    }
-
-    public boolean isNotContains(String input, String containInput) {
-        return input.contains(containInput);
     }
 }
