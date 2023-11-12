03488a803820c6541bbd616788c3da929d5fed8f
Dawin <dawin002@naver.com>
2023-11-12 18:39:25 +0900

    refactor(InputView): 입력 검증 기능 수정
    날짜 입력값 검증 기능 InputValidator 클래스로 이동:
    - validateInputDate()
    주문 입력값 검증 기능 InputValidator 클래스로 이동:
    - validateInputOrder()

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -6,15 +6,15 @@ import christmas.validations.InputValidator;
 import java.util.List;
 
 public class InputView {
-    private InputValidator validator;
+    private InputValidator inputValidator;
 
-    public InputView(InputValidator validator) {
-        this.validator = validator;
+    public InputView(InputValidator inputValidator) {
+        this.inputValidator = inputValidator;
     }
 
     public int readDate() {
         String input = Console.readLine();
-        validateInputDate(input);
+        inputValidator.validateDate(input);
         return Integer.parseInt(input);
     }
 
@@ -22,7 +22,9 @@ public class InputView {
         String input = Console.readLine();
         List<String> orders = makeInputToList(input);
         for (String order : orders) {
-            validateInputOrder(order);
+            inputValidator.validateOrder(order);
+            inputValidator.validateMenu(getMenuName(order));
+            inputValidator.validateQuantity(getMenuQuantity(order));
         }
         return orders;
     }
@@ -30,23 +32,6 @@ public class InputView {
     private List<String> makeInputToList(String input) {
         String[] splitInput = input.split(",");
         return List.of(splitInput);
-    }
-
-    private void validateInputDate(String input) {
-        if (validator.isBlank(input)
-                || validator.isNotNumber(input)) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-        }
-    }
-
-    private void validateInputOrder(String order) {
-        if (validator.isBlank(order)
-                || validator.isNotContains(order, "-")
-                || validator.isBlank(getMenuName(order))
-                || validator.isBlank(getMenuQuantity(order))
-                || validator.isNotNumber(getMenuQuantity(order))) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-        }
     }
 
     private String getMenuName(String order) {
