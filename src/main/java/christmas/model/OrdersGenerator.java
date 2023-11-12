1b6da4dd564e2ec9f07d97987f91088f1de5685c
Dawin <dawin002@naver.com>
2023-11-12 21:04:42 +0900

    feat(OrdersGenerator): 주문 등록 기능 추가
    전체 주문 리스트 초기화하는 기능:
    - OrdersGenerator()
    하나의 주문을 생성하는 기능:
    - initOrder()
    중복된 주문이 존재하는지 검사하는 기능:
    - validateDuplicate()
    음료 메뉴만 존재하는지 검사하는 기능:
    - validateOnlyDrink()
    전체 주문 수량이 20개 미만인지 검사하는 기능:
    - validateTotalQuantity()

---
+++ src/main/java/christmas/model/OrdersGenerator.java
@@ -0,0 +1,64 @@
+package christmas.model;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class OrdersGenerator {
+    private List<Order> orders;
+
+    public OrdersGenerator(List<String> inputOrders) {
+        orders = new ArrayList<>();
+        for (String inputOrder : inputOrders) {
+            Order newOrder = initOrder(inputOrder);
+            orders.add(newOrder);
+        }
+        validateDuplicate();
+        validateOnlyDrink();
+        validateTotalQuantity();
+    }
+
+    private Order initOrder(String inputOrder) {
+        String[] splitOrder = inputOrder.split("-");
+        String menu = splitOrder[0];
+        int quantity = Integer.parseInt(splitOrder[1]);
+        return new Order(menu, quantity);
+    }
+
+    private void validateDuplicate() {
+        List<String> names = new ArrayList<>();
+        HashSet<String> nonDuplicateNames = new HashSet<>();
+        for (Order order : orders) {
+            String name = order.getName();
+            names.add(name);
+            nonDuplicateNames.add(name);
+        }
+        if (names.size() != nonDuplicateNames.size()) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void validateOnlyDrink() {
+        HashSet<String> types = new HashSet<>();
+        for (Order order : orders) {
+            types.add(order.getType());
+        }
+        if (types.size() == 1 && types.contains("drink")) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void validateTotalQuantity() {
+        int totalQuantity = 0;
+        for (Order order : orders) {
+            totalQuantity += order.getQuantity();
+        }
+        if (totalQuantity > 20) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public List<Order> getOrders() {
+        return this.orders;
+    }
+}
