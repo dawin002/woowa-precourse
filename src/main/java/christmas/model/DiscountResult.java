858c39f079224aeff12627a8fe41c787a9eb5617
Dawin <dawin002@naver.com>
2023-11-13 21:31:52 +0900

    refactor(DiscountResult): 혜택 결과 클래스 이름 변경
    혜택 결과 클래스 이름 변경:
    EventResult -> DiscountResult

---
+++ src/main/java/christmas/model/DiscountResult.java
@@ -0,0 +1,11 @@
+package christmas.model;
+
+public class DiscountResult {
+    private String discountName;
+    private int totalDiscount;
+
+    public DiscountResult(String discountName, int totalDiscount) {
+        this.discountName = discountName;
+        this.totalDiscount = totalDiscount;
+    }
+}
