4e169e6718749b296f87c17f8053524ca7abdc2a
Dawin <dawin002@naver.com>
2023-11-13 15:45:09 +0900

    feat(Event): 크리스마스 디데이 할인 계산 기능 추가
    크리스마스 디데이 할인 대상인지 검사하는 기능:
    - isChristmasDDay()
    크리스마스 디데이 할인 금액 계산 결과를 반환하는 기능:
    - getChristmasDDayDiscount()

--- src/main/java/christmas/model/Event.java
+++ src/main/java/christmas/model/Event.java
@@ -1,19 +1,40 @@
 package christmas.model;
 
-public class Event {
+import christmas.model.enums.EventEnum;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Event {
     private int date;
     private Orders orders;
     private boolean eventApplicable;
 
-    public Event(int date, Orders orders) {
+    public Event(int date, Orders orders, int totalPrice) {
         this.date = date;
         this.orders = orders;
-        this.eventApplicable = isEventApplicable(orders.getTotalPrice());
+        this.eventApplicable = isEventApplicable(totalPrice);
     }
 
-    public boolean isEventApplicable(int totalPrice) {
+    private boolean isEventApplicable(int totalPrice) {
         return totalPrice >= 10000;
+    }
+
+    public EventResult getChristmasDDayDiscount() {
+        if (!eventApplicable || !isChristmasDDay()) {
+            return null;
+        }
+        String eventName = EventEnum.CHRISTMAS_DDAY.getName();
+        int totalDiscount = EventEnum.CHRISTMAS_DDAY.getPrice() * (date - 1) + 1000;
+        return new EventResult(eventName, totalDiscount);
+    }
+
+    private boolean isChristmasDDay() {
+        return date >= 1 && date <= 25;
+    }
+
+    private boolean isWeekDay(int date) {
+
     }
 
 }
