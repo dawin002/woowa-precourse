e5dd5fb8e1ba3d91ad7910c2caa9080252ece681
Dawin <dawin002@naver.com>
2023-11-13 15:47:42 +0900

    feat(EventResult): 혜택 내역 객체 추가
    혜택 내역을 저장하는 객체 추가:
    - eventName: 혜택 이름
    - totalDiscount: 혜택 할인 금액

---
+++ src/main/java/christmas/model/EventResult.java
@@ -0,0 +1,11 @@
+package christmas.model;
+
+public class EventResult {
+    private String eventName;
+    private int totalDiscount;
+
+    public EventResult(String eventName, int totalDiscount) {
+        this.eventName = eventName;
+        this.totalDiscount = totalDiscount;
+    }
+}
