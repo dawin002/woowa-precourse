61bae0bef2222789e061ad7766865f46bfbfb8a5
Dawin <dawin002@naver.com>
2023-11-13 23:00:56 +0900

    feat(Date): 날짜 반환 기능 추가
    날짜 반환 기능 추가:
    - getDate()

--- src/main/java/christmas/model/Date.java
+++ src/main/java/christmas/model/Date.java
@@ -13,4 +13,8 @@ public class Date {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
     }
+
+    public int getDate() {
+        return this.date;
+    }
 }
