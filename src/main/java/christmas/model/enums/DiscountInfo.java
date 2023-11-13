e9e87ebcdb9d3a7ffebfb689675df86593eb6e7f
Dawin <dawin002@naver.com>
2023-11-13 21:30:09 +0900

    refactor(DiscountInfo): 혜택 정보 enum 클래스 이름 변경
    혜택 정보 enum 클래스 이름 변경:
    EventInfo -> DiscountInfo

--- src/main/java/christmas/model/enums/DiscountInfo.java
+++ src/main/java/christmas/model/enums/DiscountInfo.java
@@ -1,6 +1,6 @@
 package christmas.model.enums;
 
-public enum EventInfo {
+public enum DiscountInfo {
     CHRISTMAS_DDAY("크리스마스 디데이 할인", "once", 100),
     WEEK_DAY("평일 할인", "desert", 2023),
     WEEK_END("주말 할인", "main", 2023),
@@ -9,7 +9,7 @@ public enum EventInfo {
     private String type;
     private int price;
 
-    EventInfo(String name, String type, int price) {
+    DiscountInfo(String name, String type, int price) {
         this.name = name;
         this.type = type;
         this.price = price;
