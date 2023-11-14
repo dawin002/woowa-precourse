b3813927021c4130f77bb8441747ff861213f5dc
Dawin <dawin002@naver.com>
2023-11-14 17:55:01 +0900

    refactor(DiscountDetails): 혜택 정보 저장 및 반환 기능 수정
    혜택 정보 저장 기능 추가:
    - addDiscount()
    혜택 정보 반환 기능 이동:
    - getTotalDiscountAmount()
    - DiscountService 클래스에서 분리
    혜택 내역 반환 기능 추가:
    - getDetails()
    - HashMap<String, Integer> 타입으로 반환

---
+++ src/main/java/christmas/model/DiscountDetails.java
@@ -0,0 +1,37 @@
+package christmas.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class DiscountDetails {
+    private List<Discount> discounts;
+
+    public DiscountDetails() {
+        this.discounts = new ArrayList<>();
+    }
+
+    public void addDiscount(Discount discount) {
+        if (discount != null) {
+            this.discounts.add(discount);
+        }
+    }
+
+    public HashMap<String, Integer> getDetails() {
+        HashMap<String, Integer> discountResult = new HashMap<>();
+        for (Discount discount : discounts) {
+            String discountName = discount.getName();
+            int discountAmount = discount.getAmount();
+            discountResult.put(discountName, discountAmount);
+        }
+        return discountResult;
+    }
+
+    public int getTotalDiscountAmount() {
+        int totalAmount = 0;
+        for (Discount discount : discounts) {
+            totalAmount += discount.getAmount();
+        }
+        return totalAmount;
+    }
+}
