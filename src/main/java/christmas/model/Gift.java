90d5460dedb401e93aff2f0a75a075cbae313224
Dawin <dawin002@naver.com>
2023-11-15 16:49:02 +0900

    refactor(Gift): 증정품 정보 객체 수정
    증정품 정보 초기화 기능:
    - Gift()
    증정품 정보 반환 기능:
    - getName()
    - getQuantity()

--- src/main/java/christmas/model/Gift.java
+++ src/main/java/christmas/model/Gift.java
@@ -2,42 +2,16 @@ package christmas.model;
 
 import christmas.model.enums.Menu;
 
-import java.util.HashMap;
-
 public class Gift {
     private String name;
     private int quantity;
 
-    public Gift(int totalPrice) {
-        this.name = null;
-        this.quantity = 0;
-        setPresent(totalPrice);
+    public Gift(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
     }
 
-    private void setPresent(int totalPrice) {
-        if (isChampagne(totalPrice)) {
-            setChampagne();
-        }
-    }
-
-    private boolean isChampagne(int totalPrice) {
-        return totalPrice >= 120000;
-    }
-
-    private void setChampagne() {
-        this.name = Menu.CHAMPAGNE.getName();
-        this.quantity = 1;
-    }
-
-    public HashMap<String, Integer> getGiftMenu() {
-        HashMap<String, Integer> gifts = new HashMap<>();
-        if (name != null) {
-            gifts.put(name, quantity);
-        }
-        return gifts;
-    }
-
-    public String getGiftName() {
+    public String getName() {
         return this.name;
     }
 
