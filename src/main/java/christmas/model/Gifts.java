413a5640e5357d33f7845601f735d0507181eb45
Dawin <dawin002@naver.com>
2023-11-15 16:51:16 +0900

    feat(Gifts): 증정품 관리 기능 추가
    증정품 리스트 초기화 기능:
    - Gifts()
    증정 여부 검사 기능:
    - getApplicable()
    증정품 검사 기능:
    - checkGifts()
    증정품(샴페인) 검사 기능:
    - checkChampagne()
    증정품 내역 반환 기능:
    - getGiftCount()

---
+++ src/main/java/christmas/model/Gifts.java
@@ -0,0 +1,44 @@
+package christmas.model;
+
+import christmas.model.enums.Menu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Gifts {
+    private List<Gift> gifts;
+    private boolean isApplicable;
+
+    public Gifts(int totalPrice) {
+        this.gifts = new ArrayList<>();
+        this.isApplicable = getApplicable(totalPrice);
+        checkGifts(totalPrice);
+    }
+
+    private boolean getApplicable(int totalPrice) {
+        return totalPrice >= 10000;
+    }
+
+    private void checkGifts(int totalPrice) {
+        if (isApplicable) {
+            checkChampagne(totalPrice);
+        }
+    }
+
+    private void checkChampagne(int totalPrice) {
+        if (totalPrice >= 120000) {
+            String giftName = Menu.CHAMPAGNE.getName();
+            int giftPrice = Menu.getPriceByName(giftName);
+            Gift newGift = new Gift(giftName, giftPrice);
+            gifts.add(newGift);
+        }
+    }
+
+    public int getGiftCount() {
+        return gifts.size();
+    }
+
+    public List<Gift> getGifts() {
+        return gifts;
+    }
+}
